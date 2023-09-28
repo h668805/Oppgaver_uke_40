964e4d26632c0dcb6c39c0769933f6a425756faf
h668805 <668805@stud.hvl.no>
2023-09-28 08:39:03 +0200

    ferdig

--- startcode/src/no/hvl/dat100/varelager/Varelager.java
+++ startcode/src/no/hvl/dat100/varelager/Varelager.java
@@ -4,43 +4,50 @@ public class Varelager {
 
 	protected Vare[] varer;
 	protected int antall;
-	
+
 	public Varelager(int n) {
-		
-		throw new TODO("Varelager");
-
+		this.varer = new Vare[n];
+		this.antall = 0;
 	}
-	
+
 	public Vare[] getVarer() {
-		
-		throw new TODO("getVarer");
 
+		return this.varer;
 	}
-	
+
 	public boolean leggTilVare(Vare v) {
-		
-		throw new TODO("leggTilVare");
-
+		if (antall < this.varer.length) {
+			this.varer[antall] = v;
+			antall++;
+			return true;
+		} else
+			return false;
 	}
-	
+
 	public boolean leggTil(int varenr, String navn, double pris) {
-		
-		throw new TODO("leggTil");
+		Vare v = new Vare(varenr, navn, pris);
 
+		return leggTilVare(v);
 	}
-	
+
 	public Vare finnVare(int varenr) {
-			
-		throw new TODO("finnVare");
-
+		for (int i = 0; i < this.varer.length; i++) {
+			if (this.varer[i].getVarenr() == varenr)
+				return varer[i];
+		}
+		return null;
 	}
-	
-	private String SEP = "==============================";
-	
+
+	private String SEP = "=================================";
+
 	public void printVarelager() {
-				
-		throw new TODO("printVarelager");
-
+		System.out.println(this.SEP);
+		for (int i = 0; i < this.varer.length; i++) {
+			System.out.print("Vare [varenr=" + i);
+			System.out.print(", navn=" + varer[i].getNavn());
+			System.out.println(", pris=" + varer[i].getPris() + "]");
+		}
+		System.out.println(this.SEP);
 	}
-	
+
 }
--- startcode/src/no/hvl/dat100/varelager/VarelagerUtils.java
+++ startcode/src/no/hvl/dat100/varelager/VarelagerUtils.java
@@ -3,26 +3,39 @@ package no.hvl.dat100.varelager;
 public class VarelagerUtils {
 
 	public static Vare finnBilligste(Vare[] varer) {
-		
-		throw new TODO("finnBilligste");
+		int minI = 0;
 
-	}
-	
-	public static double totalPris(Vare[] varer) {		
-		
-		throw new TODO("totalPris");
+		for (int i = 1; i < varer.length; i++)
+			if (varer[i].getPris() < varer[minI].getPris())
+				minI = i;
 
+		return varer[minI];
 	}
-	
+
+	public static double totalPris(Vare[] varer) {
+		double sum = 0;
+
+		for (Vare vare : varer)
+			sum += vare.getPris();
+
+		return sum;
+	}
+
 	public static int[] finnVarenr(Vare[] varer) {
-				
-		throw new TODO("finnVarenr");
+		int[] varenr = new int[varer.length];
 
+		for (int i = 0; i < varer.length; i++)
+			varenr[i] = varer[i].getVarenr();
+
+		return varenr;
 	}
-	
-	public static double[] finnPrisDifferanser(Vare[] varer) {
-				
-		throw new TODO("finnPrisDifferanser");
 
+	public static double[] finnPrisDifferanser(Vare[] varer) {
+		double[] prisDiff = new double[varer.length - 1];
+		
+		for (int i = 0; i < varer.length - 1; i++)
+			prisDiff[i] = varer[i+1].getPris() - varer[i].getPris();
+		
+		return prisDiff;
 	}
 }
